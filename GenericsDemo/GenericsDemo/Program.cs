a6c10766d03cec65f2c1f8c313f4d308194bfa2d
Eric Perry <eric.s.perry@outlook.com>
2017-05-15 22:23:23 -0400

    Created various Data Structures with C# Generics

---
+++ GenericsDemo/GenericsDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GenericsDemo.EnumerableCompositor;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+
+        static void Main(string[] args)
+        { 
+            var list1 = new List<int> { 1, 2, 3, 4, 5 };
+            var list2 = new List<int> { 2, 4, 6, 8, 10 };
+            var set1 = new HashSet<int> { 3, 6, 9, 12, 15 };
+            var array1 = new[] { 4, 8, 12, 16, 20 };
+
+            int numOdd = EC(list1, list2, set1, array1).Count(x => IsOdd(x));
+
+            HashSet<int> set = EC(list1, list2, set1, array1).To<HashSet<int>>();
+
+        }
+    }
+}
---
+++ GenericsDemo/GenericsDemo/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public static class Utils
+    {
+        public static IEnumerable<T> Take<T>(IEnumerable<T> source, int n)
+        {
+            int i = 0;
+
+            foreach(var item in source)
+            {
+                yield return item;
+
+                if(++i == n)
+                {
+                    yield break;
+                }
+            }
+        }
+
+        public static T Min<T>(T item1, T item2) where T : IComparable<T>
+        {
+            return (item1.CompareTo(item2) < 0) ? item1 : item2;
+        }
+    }
+}
